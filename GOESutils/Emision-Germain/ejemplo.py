a1cfbaa9cf99335d3d915fbd8b23085b0169a04c
Germain Rosadio Vega <75147555+GermainRV@users.noreply.github.com>
2023-11-16 00:57:08 -0500

    Delete GOESutils directory

--- GOESutils/Emision-Germain/ejemplo.py
+++
@@ -1,7 +0,0 @@
-from comentarios import enviar
-
-enviar("producto_uno_Lima","Comentario")
-enviar("producto_dos_Lima","Comentario")
-enviar("producto_tres_Lima","Comentario")
-enviar("producto_cuatro_Lima","Comentario")
-
